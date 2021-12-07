63b60ab05cbb230bf75e97399040921b7daf71b9
std4453 <me@std4453.com>
2021-12-07 13:52:36 +0800

    Add original version of modified files
    Jellyfin base is a28430bd33ab71dfc24167ce4d22cc846dff7319
    
    This commit MUST be the first commit!

---
+++ webpack.common.js
@@ -0,0 +1,175 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { DefinePlugin } = require('webpack');
+
+const Assets = [
+    'native-promise-only/npo.js',
+    'libarchive.js/dist/worker-bundle.js',
+    'libass-wasm/dist/js/subtitles-octopus-worker.js',
+    'libass-wasm/dist/js/subtitles-octopus-worker.data',
+    'libass-wasm/dist/js/subtitles-octopus-worker.wasm',
+    'libass-wasm/dist/js/subtitles-octopus-worker-legacy.js',
+    'libass-wasm/dist/js/subtitles-octopus-worker-legacy.data',
+    'libass-wasm/dist/js/subtitles-octopus-worker-legacy.js.mem',
+    'pdfjs-dist/build/pdf.worker.js'
+];
+
+const LibarchiveWasm = [
+    'libarchive.js/dist/wasm-gen/libarchive.js',
+    'libarchive.js/dist/wasm-gen/libarchive.wasm'
+];
+
+module.exports = {
+    context: path.resolve(__dirname, 'src'),
+    target: 'browserslist',
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+        modules: [
+            path.resolve(__dirname, 'node_modules')
+        ]
+    },
+    plugins: [
+        new DefinePlugin({
+            __WEBPACK_SERVE__: JSON.stringify(!!process.env.WEBPACK_SERVE)
+        }),
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: 'index.html',
+            // Append file hashes to bundle urls for cache busting
+            hash: true
+        }),
+        new CopyPlugin({
+            patterns: [
+                {
+                    from: 'themes/',
+                    to: 'themes/'
+                },
+                {
+                    from: 'assets/**',
+                    globOptions: {
+                        dot: true,
+                        ignore: ['**/css/*']
+                    }
+                },
+                {
+                    from: '*.*',
+                    globOptions: {
+                        dot: true,
+                        ignore: ['**.js', '**.html']
+                    }
+                }
+            ]
+        }),
+        new CopyPlugin({
+            patterns: Assets.map(asset => {
+                return {
+                    from: path.resolve(__dirname, `./node_modules/${asset}`),
+                    to: path.resolve(__dirname, './dist/libraries')
+                };
+            })
+        }),
+        new CopyPlugin({
+            patterns: LibarchiveWasm.map(asset => {
+                return {
+                    from: path.resolve(__dirname, `./node_modules/${asset}`),
+                    to: path.resolve(__dirname, './dist/libraries/wasm-gen')
+                };
+            })
+        })
+    ],
+    output: {
+        filename: '[name].jellyfin.bundle.js',
+        chunkFilename: '[name].[contenthash].chunk.js',
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: ''
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(html)$/,
+                use: {
+                    loader: 'html-loader'
+                }
+            },
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules[\\/](?!@uupaa[\\/]dynamic-import-polyfill|blurhash|date-fns|epubjs|flv.js|libarchive.js|marked|screenfull)/,
+                use: [{
+                    loader: 'babel-loader'
+                }]
+            },
+            {
+                test: /\.(ts|tsx)$/,
+                exclude: /node_modules/,
+                use: [{
+                    loader: 'ts-loader'
+                }]
+            },
+            /* modules that Babel breaks when transforming to ESM */
+            {
+                test: /node_modules[\\/](pdfjs-dist|xmldom)[\\/].*\.js$/,
+                use: [{
+                    loader: 'babel-loader',
+                    options: {
+                        plugins: [
+                            '@babel/transform-modules-umd'
+                        ]
+                    }
+                }]
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            postcssOptions: {
+                                config: path.resolve(__dirname, 'postcss.config.js')
+                            }
+                        }
+                    },
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.css$/i,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            postcssOptions: {
+                                config: path.resolve(__dirname, 'postcss.config.js')
+                            }
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(png|jpg|gif|svg)$/i,
+                type: 'asset/resource'
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                type: 'asset/resource'
+            },
+            {
+                test: /\.(mp3)$/i,
+                type: 'asset/resource'
+            },
+            {
+                test: require.resolve('jquery'),
+                loader: 'expose-loader',
+                options: {
+                    exposes: ['$', 'jQuery']
+                }
+            }
+        ]
+    }
+};
